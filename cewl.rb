91e6ad9876f26f165f3170860298a116b338ba84
Robin <robin@digi.ninja>
2022-09-29 14:00:53 +0100

    only try and check HTTP based links

--- cewl.rb
+++ cewl.rb
@@ -169,7 +169,11 @@ class MySpiderInstance<SpiderInstance
 
 				y = []
 				x.select do |a_url, parsed_url|
-					y << [a_url, parsed_url] if allowable_url?(a_url, parsed_url)
+                    puts parsed_url.inspect
+                    puts parsed_url.scheme.inspect
+                    if (parsed_url.scheme == "http" or parsed_url.scheme == "https") then
+                      y << [a_url, parsed_url] if allowable_url?(a_url, parsed_url)
+                    end
 				end
 
 				y.each do |a_url, parsed_url|
